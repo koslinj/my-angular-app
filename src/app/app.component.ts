4eab690cb4066e9ba1a8a27f1b56862c6eb0e68b
koslinj <koslinj@gmail.com>
2022-06-12 17:04:32 +0200

    12.06

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,81 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
+import { Student } from './student';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms"
+import { PasswordStrengthValidator } from "./password-strengh.validator"
+import { EmailStrengthValidator } from './password-strengh.validator'
+import { NameStrengthValidator } from './password-strengh.validator'
+import { SurnameStrengthValidator } from './password-strengh.validator'
+import { NickStrengthValidator } from './password-strengh.validator'
+import { TelStrengthValidator } from "./password-strengh.validator"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  public form: FormGroup;
+
+  constructor(fb: FormBuilder) {
+
+    this.form = fb.group({
+      password: ['', [Validators.required, PasswordStrengthValidator]],
+      email: ['', [Validators.required, EmailStrengthValidator]],
+      name: ['', [Validators.required, NameStrengthValidator]],
+      surname: ['', [Validators.required, SurnameStrengthValidator]],
+      nick: ['', [Validators.required, NickStrengthValidator]],
+      tel: ['', [Validators.required, TelStrengthValidator]],
+      date: ['', [Validators.required]],
+      gender: ['', [Validators.required]]
+    });
+
+
+
+  }
+
+  studenci: Student[] = [];
+  
+  dodajStudenta(event: any){
+    event.preventDefault();
+    const daneStudenta = {
+      imie: event.target.imie.value,
+      nazwisko: event.target.nazwisko.value,
+      nick: event.target.nick.value,
+      haslo: event.target.haslo.value,
+      email: event.target.email.value,
+    };
+    this.studenci.push(daneStudenta);
+  }
 
   games: {name: string, img: string}[] = [
     { name: 'Wybierz gre...', img: '' },
     { name: 'Counter Strike: Global Offensive', img: 'assets/csgo.jpg' },
     { name: 'League of Legends', img: 'assets/lol.jpg' },
     { name: 'FIFA 22', img: 'assets/fifa.jpg' },
-    { name: 'Dota2', img: 'assets/dota.jpg' },
-    { name: 'Smite', img: 'assets/smite.jpg' }
+    { name: 'Fortnite', img: 'assets/fortnite.jpg' },
   ];
 
   selectedGame: {name: string, img: string} = this.games[0];
 
   title = 'my-angular-app';
 
+  regions: {name: string}[] = [
+    { name: 'Wybierz region...'},
+    { name: 'EUROPA'},
+    { name: 'AMERYKA'},
+    { name: 'AFRYKA'},
+    { name : 'AZJA'},
+  ];
+  selectedRegion: {name: string} = this.regions[0];
   
+  public show:boolean = false;
+  public buttonName:any = 'Show';
+
+  ngOnInit () {  }
+
+  toggle() {
+    this.show = !this.show;
+  }
+
 }
---
+++ src/app/password-strengh.validator.ts
@@ -0,0 +1,136 @@
+import { AbstractControl, ValidationErrors } from "@angular/forms"
+
+export const PasswordStrengthValidator = function (control: AbstractControl): ValidationErrors | null {
+
+  let value: string = control.value || '';
+
+  if (!value) {
+    return null
+  }
+
+  let upperCaseCharacters = /[A-Z]+/g
+  if (upperCaseCharacters.test(value) === false) {
+    return { passwordStrength: `Hasło musi zawierać wielką literę` };
+  }
+
+  let lowerCaseCharacters = /[a-z]+/g
+  if (lowerCaseCharacters.test(value) === false) {
+    return { passwordStrength: `Hasło musi zawierać małą literę` };
+  }
+
+
+  let numberCharacters = /[0-9]+/g
+  if (numberCharacters.test(value) === false) {
+    return { passwordStrength: `Hasło musi zawierać cyfrę` };
+  }
+
+
+  let specialCharacters = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/
+  if (specialCharacters.test(value) === false) {
+    return { passwordStrength: `Hasło musi zawierać znak specjalny` };
+  }
+
+  if (value.length < 8) {
+    return { passwordStrength: `Hasło jest za krótkie` };
+  }
+  
+
+  return null;
+}
+
+export const EmailStrengthValidator = function (control: AbstractControl): ValidationErrors | null {
+
+  let value: string = control.value || '';
+
+  if (!value) {
+    return null
+  }
+
+  let malpa = /\@/g
+  if (malpa.test(value) === false) {
+    return { emailStrength: `Email musi zawierać znak "@"` };
+  }
+
+  let kropka = /\./g
+  if (kropka.test(value) === false) {
+    return { emailStrength: `Email musi zawierać znak "."` };
+  }
+  
+  if (value.length < 10) {
+    return { emailStrength: `Email jest za krótkie` };
+  }
+
+  return null;
+}
+
+export const NameStrengthValidator = function (control: AbstractControl): ValidationErrors | null {
+
+  let value: string = control.value || '';
+
+  if (!value) {
+    return null
+  }
+
+  let upperCaseCharacters = /[A-Z]+/g
+  if (upperCaseCharacters.test(value) === false) {
+    return { nameStrength: `Imię musi zawierać wielką literę` };
+  }
+
+  return null;
+}
+
+export const SurnameStrengthValidator = function (control: AbstractControl): ValidationErrors | null {
+
+  let value: string = control.value || '';
+
+  if (!value) {
+    return null
+  }
+
+  let upperCaseCharacters = /[A-Z]+/g
+  if (upperCaseCharacters.test(value) === false) {
+    return { SurnameStrength: `Nazwisko musi zawierać wielką literę` };
+  }
+
+  return null;
+}
+
+export const NickStrengthValidator = function (control: AbstractControl): ValidationErrors | null {
+
+  let value: string = control.value || '';
+
+  if (!value) {
+    return null
+  }
+
+  if (value.length < 3) {
+    return { NickStrength: `Nick jest za krótki` };
+  }
+
+  return null;
+}
+
+export const TelStrengthValidator = function (control: AbstractControl): ValidationErrors | null {
+
+  let value: string = control.value || '';
+
+  var numbers = /^[0-9]+$/;
+
+  if (!value) {
+    return null
+  }
+
+  if(!numbers.test(value)){
+    return { TelStrength: `Numer telefonu powinien zawierać tylko cyfry` };
+  }
+
+  if (value.length < 9) {
+    return { TelStrength: `Numer telefonu jest za krótki` };
+  }
+
+  if (value.length > 9) {
+    return { TelStrength: `Numer telefonu jest za długi` };
+  }
+
+  return null;
+}
